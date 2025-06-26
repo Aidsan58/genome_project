250521fe057373cb20cd5ac9ce011aa8b962fc24
Aidsan58 <80861911+Aidsan58@users.noreply.github.com>
2025-06-26 14:17:40 -0700

    Update final_exam.py
    Changed longest_sequence to sequence_length, adding capability to locate and describe the shortest sequence.

--- final_exam.py
+++ final_exam.py
@@ -10,28 +10,36 @@ def header_count(filename):
     
     return print('The total number of headers in the file is ' + str(count) + ".")
 
-def longest_sequence(filename):
-    """ Finds the longest DNA sequence. """
+def sequence_length(filename):
+    """ Finds the longest DNA sequence, and the shortest DNA sequence. """
     current_sequence = ''
     longest_sequence = 0
+    shortest_sequence = 2300000
     with open(filename, 'r') as f:
         for line in f:
             if line.startswith('>'):
                 if len(current_sequence) > longest_sequence:
                     longest_sequence = len(current_sequence)
+                if len(current_sequence) > 0:
+                    if len(current_sequence) < shortest_sequence:
+                        shortest_sequence = len(current_sequence)
                 current_sequence = ''
             else:
                 current_sequence += line.strip()
 
         if len(current_sequence) > longest_sequence:
             longest_sequence = len(current_sequence)
+            
+        if len(current_sequence) < shortest_sequence:
+            shortest_sequence = len(current_sequence)
+            
 
         
-    return print('The longest sequence has a length of ' + str(longest_sequence) + '.')
+    return print('The longest sequence has a length of ' + str(longest_sequence) + '. The shortest sequence has a length of ' + str(shortest_sequence) + '.')
 
 
 file_path = 'D:/Programs/Python Programs/Python for Genomics/dna.example.fasta'
 
 header_count(file_path)
 
-longest_sequence(file_path)
+sequence_length(file_path)
